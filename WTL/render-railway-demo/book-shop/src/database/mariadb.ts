150b31eb6755ff0157ba9b8afc92a2a80baae28e
FF <wjdrldud98@gmail.com>
2025-05-16 12:44:30 +0900

    fix: database 이름 수정

--- WTL/render-railway-demo/book-shop/src/database/mariadb.ts
+++ WTL/render-railway-demo/book-shop/src/database/mariadb.ts
@@ -8,7 +8,7 @@ export const connection = mariadb.createPool({
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "Bookshop",
+  database: "railway",
   waitForConnections: true,
   connectionLimit: 10,
   dateStrings: true,
