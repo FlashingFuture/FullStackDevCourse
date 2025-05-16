3c11d8cb0b409ab3fdbed8cb105379e8b951807f
FF <wjdrldud98@gmail.com>
2025-05-16 12:41:22 +0900

    chore: DB PORT 추가

--- WTL/render-railway-demo/book-shop/src/app.ts
+++ WTL/render-railway-demo/book-shop/src/app.ts
@@ -7,7 +7,7 @@ import { errorHandler } from "./middlewares/errorHandler";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/users", userRouter);
